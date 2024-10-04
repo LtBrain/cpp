6e8a38b928f57322b0f1ad6fe4341ab29a06ac0d
LtBrain <brian.j.guan@outlook.com>
2024-10-04 15:50:54 +0000

    It in fact did not work

--- Brightfield/Brightfield/src/main.cpp
+++ Brightfield/Brightfield/src/main.cpp
@@ -43,10 +43,10 @@ std::vector<Token> tokenize(const std::string& str) {
             }
         }
         else if (std::isdigit(c)) {
-            buf.push_back(c);
+            buf.push_back(i);
             i++;
-            while (std::isdigit(str.at(c))) {
-                buf.push_back(str.at(c));
+            while (std::isdigit(str.at(i))) {
+                buf.push_back(str.at(i));
                 i++;
             }
             i--;
@@ -63,6 +63,26 @@ std::vector<Token> tokenize(const std::string& str) {
             exit(EXIT_FAILURE);
         }
     }
+
+    return tokens;
+}
+
+std::string tokens_to_asm(const std::vector<Token>& tokens) {
+    std::stringstream output;
+    output << "global _start\nstart:\n";
+    for(int i = 0; i < tokens.size(); i++) {
+        const Token& token = tokens.at(i);
+        if (token.type == TokenType::_return) {
+            if (i + 1 < tokens.size() && tokens.at(i + 1).type == TokenType::int_lit) {
+                if(i + 2 < tokens.size() && tokens.at(i + 2).type == TokenType::semi) {
+                    output << "    mov rax, 60\n";
+                    output << "    mov rdi, " << tokens.at(i + 1).value.value() << "\n";
+                    output << "    syscall";
+                }
+            }
+        }
+    }
+    return output.str();
 }
 
 int main(int argc, char* argv[])
@@ -81,7 +101,8 @@ int main(int argc, char* argv[])
         contents = contents_stream.str();
     }
 
-    std::vector<Token> thing = tokenize(contents);
+    std::vector<Token> tokens = tokenize(contents);
+    std::cout << tokens_to_asm(tokens) << std::endl;
 
-    std::cout << contents <<std::endl;
+    return EXIT_SUCCESS;
 }
