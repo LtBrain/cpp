559ab9ad9593a81a8eb0c5bd9810f57d6fc2d2e7
LtBrain <brian.j.guan@outlook.com>
2024-10-05 15:24:32 +0000

    Uhhhhh Its Sorta Better ig???

--- Brightfield/Brightfield/src/main.cpp
+++ Brightfield/Brightfield/src/main.cpp
@@ -8,64 +8,11 @@
 #include <sstream>
 #include <vector>
 
-enum class TokenType {
-    _return,
-    int_lit,
-    semi
-};
+#include "./tokenization.hpp"
 
-struct Token {
-    TokenType type;
-    std::optional<std::string> value {};
-};
 
 std::vector<Token> tokenize(const std::string& str) {
-    std::vector<Token> tokens;
-
-    std::string buf;
-    for (int i = 0; i < str.length(); i++) {
-        char c = str.at(i);
-        if (std::isalpha(c)) {
-            buf.push_back(c);
-            i++;
-            while (std::isalnum(str.at(i))) {
-                buf.push_back(str.at(i));
-                i++;
-            }
-            i--;
-
-            if (buf == "return") {
-                tokens.push_back({.type = TokenType::_return});
-                buf.clear();
-                continue;
-            } else {
-                std::cerr << "Uh Oh, Something's Wrong!" << std::endl;
-                exit(EXIT_FAILURE);
-            }
-        }
-        else if (std::isdigit(c)) {
-            buf.push_back(c);
-            i++;
-            while (std::isdigit(str.at(i))) {
-                buf.push_back(str.at(i));
-                i++;
-            }
-            i--;
-            tokens.push_back({.type = TokenType::int_lit, .value = buf});
-            buf.clear();
-        }
-        else if (c == ';') {
-            tokens.push_back({.type = TokenType::semi});
-        }
-        else if (std::isspace(c)) {
-            continue;
-        } else {
-            std::cerr << "Uh Oh, Something's Wrong!" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    return tokens;
+ 
 }
 
 std::string tokens_to_asm(const std::vector<Token>& tokens) {
@@ -73,7 +20,7 @@ std::string tokens_to_asm(const std::vector<Token>& tokens) {
     output << "global _start\n_start:\n";
     for(int i = 0; i < tokens.size(); i++) {
         const Token& token = tokens.at(i);
-        if (token.type == TokenType::_return) {
+        if (token.type == TokenType::exit) {
             if (i + 1 < tokens.size() && tokens.at(i + 1).type == TokenType::int_lit) {
                 if(i + 2 < tokens.size() && tokens.at(i + 2).type == TokenType::semi) {
                     output << "    mov rax, 60\n";
